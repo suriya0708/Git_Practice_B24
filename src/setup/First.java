e9b2724bbe94a992950ac730a05739358f527966
suriya0708 <suriyaparhat@gmail.com>
2021-10-08 00:58:10 -0700

    set up new project, added gitignore file

--- src/setup/First.java
+++ src/setup/First.java
@@ -3,5 +3,15 @@ package setup;
 public class First {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        // this is a new line
+        // I want to create a history
+        // The text doesn't matter
+        // Third commit
+        // more lines
+
+
+
+
     }
 }
