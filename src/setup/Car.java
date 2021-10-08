25dc5541960e4b76f6ac0af65206823d93ba969b
suriya0708 <suriyaparhat@gmail.com>
2021-10-08 14:11:45 -0700

    made car in master branch

---
+++ src/setup/Car.java
@@ -0,0 +1,16 @@
+package setup;
+
+public class Car {
+    public static void main(String[] args) {
+
+
+
+        // this is car class from master branch
+
+        // we will have conflict
+
+        // because this is and exerciese
+
+
+    }
+}
